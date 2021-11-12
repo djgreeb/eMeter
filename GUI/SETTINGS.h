21fb49aefabd42a0b7458ba2a3aae9a3ed0b8e86
Andrei <greeb-mail@mail.ru>
2021-11-12 12:04:23 +0300

    Add files via upload

---
+++ GUI/SETTINGS.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+Image data created by the LCD Vision V1.05 font & image editor/converter
+(C) Copyright 2011-2013 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Graphic LCD controller: UC1701 128x32 8080 Interface
+Image width: 128 pixels
+Image height: 32 pixels
+Color depth: 1 bits/pixel
+Imported image file name: emeter_settings.bmp
+
+Exported monochrome image data size:
+516 bytes for displays organized as horizontal rows of bytes
+516 bytes for displays organized as rows of vertical bytes.
+****************************************************************************/
+
+#ifndef _SETTINGS_INCLUDED_
+#define _SETTINGS_INCLUDED_
+
+extern flash unsigned char emeter_settings[];
+
+#endif
+
---
+++ GUI/logo.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+Image data created by the LCD Vision V1.05 font & image editor/converter
+(C) Copyright 2011-2013 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Graphic LCD controller: ST7565 128x32 8080 Interface
+Image width: 128 pixels
+Image height: 32 pixels
+Color depth: 1 bits/pixel
+Imported image file name: emeter_logo.bmp
+
+Exported monochrome image data size:
+516 bytes for displays organized as horizontal rows of bytes
+516 bytes for displays organized as rows of vertical bytes.
+****************************************************************************/
+
+#ifndef _LOGO_INCLUDED_
+#define _LOGO_INCLUDED_
+
+extern flash unsigned char emeter_logo[];
+
+#endif
+
